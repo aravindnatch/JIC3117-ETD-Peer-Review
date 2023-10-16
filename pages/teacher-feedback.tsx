3811c48c153f6336bcc775029d77c386c9f40ce5
Abhi Vijaykumar <abhivijaykumar@lawn-10-91-113-58.lawn.gatech.edu>
2023-10-16 19:08:37 -0400

    added better UI especially for the loading of teacher feedback

---
+++ pages/teacher-feedback.tsx
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+
+interface TeacherFeedback {
+  teacherId: number;
+  teacherName: string;
+  feedbackGiven: Array<FeedbackItem>;
+}
+
+interface FeedbackItem {
+  feedbackId: number;
+  studentId: number;
+  studentName: string;
+  feedback: string;
+}
+
+const TeacherFeedback: React.FC = () => {
+  const [teacherFeedback, setTeacherFeedback] = useState<TeacherFeedback | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Simulated data fetching
+        const response = await fetch("/api/teacher-feedback"); // Replace with your actual API endpoint
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        const feedbackData = await response.json();
+
+        setTeacherFeedback(feedbackData);
+        setLoading(false); // Set loading to false when data is loaded
+      } catch (error) {
+        setError("An error occurred while fetching data.");
+        setLoading(false); // Set loading to false when an error occurs
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (error) {
+    return (
+      <div>
+        <h1>Teacher Feedback</h1>
+        <div>Error: {error}</div>
+      </div>
+    );
+  }
+
+  if (loading) {
+    // Display a loading spinner or animation
+    return (
+      <div>
+        <h1>Teacher Feedback</h1>
+        <div className="loading-spinner">Loading...</div>
+      </div>
+    );
+  }
+
+  if (teacherFeedback === null) {
+    return (
+      <div>
+        <h1>Teacher Feedback</h1>
+        <div>No data available.</div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Teacher Feedback</h1>
+      <h2>Feedback Given</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Feedback ID</th>
+            <th>Student</th>
+            <th>Feedback</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teacherFeedback.feedbackGiven.map((feedbackItem) => (
+            <tr key={feedbackItem.feedbackId}>
+              <td>{feedbackItem.feedbackId}</td>
+              <td>{feedbackItem.studentName}</td>
+              <td>{feedbackItem.feedback}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TeacherFeedback;
